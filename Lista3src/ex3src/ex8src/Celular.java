99a06d60c24aaca6f13e80db2498c9463640672a
Guilherme Vianna de Moura <88854722+GuilhermeViannaMoura@users.noreply.github.com>
2022-04-18 19:39:56 -0300

    Add files via upload

--- Lista3src/ex3src/ex8src/Celular.java
+++ Lista3src/ex3src/ex8src/Celular.java
@@ -1,20 +1,23 @@
 public class Celular {
-    Bateria bateria;
-    boolean estaEmChamada=false;
+    Bateria bateria = new Bateria();
+    private boolean estaEmChamada=false;
+    
     
     void enviaMsg(){
         bateria.setNivelCarga(bateria.getNivelCarga() - 1);
     }
     
     void chamada(){
-        
-        try{
-            Thread.sleep(5*1000);// aguarda 5 segundos
-        }catch(InterruptedException e){}
-        
-        bateria.setNivelCarga(bateria.getNivelCarga() - 1);
-        
         estaEmChamada = true;
+        int cont=0;
+        long startTime = System.currentTimeMillis();
+        while (estaEmChamada){
+            if ((System.currentTimeMillis()>=startTime+(cont*5000))){
+                bateria.setNivelCarga(bateria.getNivelCarga() - 1);
+                cont++;
+            }
+            if ((System.currentTimeMillis()-startTime)>=15000) encerraChamada(); // 15 segundos de chamada
+        }
     }
     
     void encerraChamada(){
--- Lista3src/ex3src/ex8src/L02Ex08.java
+++ Lista3src/ex3src/ex8src/L02Ex08.java
@@ -1,18 +1,14 @@
 public class L02Ex08 {
+
     public static void main(String[] args) {
         Celular cel = new Celular();
-        int cont=0;
         
         cel.bateria.setNivelCarga(90);
         cel.printCargaAtual();
         cel.enviaMsg();
         cel.printCargaAtual();
         
-        while (cel.estaEmChamada){
-            cel.chamada();
-            cont++;
-            if (cont>3) cel.encerraChamada(); // 15 segundos de chamada
-        }
+        cel.chamada();
         
         cel.printCargaAtual();
     }
